ddeae1e92b48702039e67f6fc6c8fc64b26746ff
Samara <Samara.Mejia@gmail.com>
2016-03-19 17:42:38 -0500

    added comments to code

--- html/chatbot/db/migrate/20160313230516_create_relationships.rb
+++ html/chatbot/db/migrate/20160313230516_create_relationships.rb
@@ -1,13 +1,13 @@
+#The migration adds a table called 'relationships' 
 class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
-		t.integer :node_a_id
-		t.integer :node_b_id
+		t.integer :node_a_id #creates an integer column called 'node a id'
+		t.integer :node_b_id #creates an integer column called 'node b id'
 		t.string :a_to_b_relationship #if A is B's parent, this would read "parent"
 		t.string :b_to_a_relationship #if A is B's parent, this would read "child"
-		t.decimal :strength
-	
-		t.timestamps
+		t.decimal :strength #creating an integer column called 'strength' that will eventually measure the strength between nodes. 	
+		t.timestamps #the timestamp column created_at and updated_at which Active Record populates automatically will be added.
     end
   end
 end
--- html/chatbot/db/migrate/20160318232857_add_bot_flag_to_messages.rb
+++ html/chatbot/db/migrate/20160318232857_add_bot_flag_to_messages.rb
@@ -1,5 +1,7 @@
+#This adds a column to the 'messages' table. Did this so didn't have to re-create the database table. 
 class AddBotFlagToMessages < ActiveRecord::Migration
   def change
-	add_column :messages, :bot_message, :boolean, :default => false
+	add_column :messages, :bot_message, :boolean, :default => false #this adds a column in the table 'messages' that comes from the robot.
+	#boolean is a binary variable that is either 'true' or 'false', in this case defaults to 'false'
   end
 end
