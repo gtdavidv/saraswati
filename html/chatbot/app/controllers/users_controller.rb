728b0e636c37e285e1c4a79bde6547be3f03b62a
Ablorde Ashigbi <ablordeashigbi@Ablordes-MacBook-Pro.local>
2016-03-26 16:46:46 -0500

    user authentication - with issues still outstanding on using the login page

---
+++ html/chatbot/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+def new
+	end
+
+def create
+	user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    	end
+    end
+
+private
+
+def user_param
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
