05f64e48b8614c00bc9604332278bd090237b467
David <gtdavidv@gmail.com>
2016-03-29 19:24:54 -0500

    Tied chats to users

--- html/chatbot/app/controllers/chat_controller.rb
+++ html/chatbot/app/controllers/chat_controller.rb
@@ -1,14 +1,26 @@
 class ChatController < ApplicationController
 	include NlqProcessor #Separate file in the lib folder that contains the code for processing queries
 	
-	before_filter :authorize
+	#before_filter :authorize
 
 	def index
 		if !(defined? session[:chat_id]) || !session[:chat_id]
-			@chat = Chat.create
+			if !(defined? session[:user_id]) || !session[:user_id]
+				@chat = Chat.create
+			else
+				if Chat.exists?(user_id: session[:user_id])
+					@chat = Chat.where('user_id' => session[:user_id])
+				else
+					@chat = Chat.create(user_id: session[:user_id])
+				end
+			end
 			session[:chat_id] = @chat.id
-		#else
-			#@chat = Chat.find_by_id(session[:chat_id])
+		else
+			if defined? session[:user_id]
+				if !Chat.exists?(user_id: session[:user_id])
+					Chat.where('id' => session[:chat_id]).update_all('user_id' => session[:user_id])
+				end
+			end
 		end
 		
 		@messages = Message.joins(:chat).where('chat_id' => session[:chat_id])	
